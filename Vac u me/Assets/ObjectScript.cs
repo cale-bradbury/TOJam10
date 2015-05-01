4de24d035a96944ab82b23962d24d627501cb018
netgrind <cale.bradbury@gmail.com>
2015-05-01 19:08:38 -0400

    vac working
    clean it up

---
+++ Vac u me/Assets/ObjectScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ObjectScript : MonoBehaviour {
+
+	public static List<ObjectScript> all = new List<ObjectScript> ();
+
+	public float weight = 1;
+	public float value = 1;
+
+	[HideInInspector]public Rigidbody body;
+
+	// Use this for initialization
+	void Start () {
+		all.Add (this);
+		body = GetComponent<Rigidbody> ();
+		if (body == null) {
+			body = gameObject.AddComponent<Rigidbody>();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
