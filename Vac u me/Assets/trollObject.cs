1501f7dba59545fa4994aa0ade3235e43d011f84
Erik Krieg <erik.c.krieg@gmail.com>
2015-05-02 11:04:05 -0400

    troll target selection and movement towards target added. Saved proj and scene.

--- Vac u me/Assets/trollObject.cs
+++ Vac u me/Assets/trollObject.cs
@@ -10,14 +10,43 @@ public class trollObject : ObjectScript {
 
 	public override void Start(){
 		base.Start ();
-		body.constraints = RigidbodyConstraints.FreezeRotationY;
-
-		//target = Game.all [Random.Range(0, Game.all.Count - 1)];
 	}
 
-	// selects a target object
+	void Update(){
+		selectTarget ();
+		moveToTarget ();
+	}
+	
+	void selectTarget(){
+		if (target == null) {
+			target = targetFoodObject();
+		}
+	}
+	
+	void moveToTarget(){
+		if (target != null) {
+			transform.position = Vector3.MoveTowards (transform.position, target.transform.position, speed * Time.deltaTime);
+		}
+	}
 
-	// moves to object
-		// destroys object
-		// selects new object
+	GameObject targetFoodObject(){
+		GameObject[] gos;
+		GameObject newTarget;
+		gos = GameObject.FindGameObjectsWithTag ("Food");
+
+		if (gos.Length > 0) {
+			newTarget = gos [Random.Range (0, gos.Length - 1)];
+		} else {
+			newTarget = null;
+		}
+
+		return newTarget;
+	}
+
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.CompareTag ("Food")) {
+			Destroy(col.gameObject);
+			target = null;
+		}
+	}
 }
