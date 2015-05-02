a81b9f967bc8598e748a52679bab6ca82a488e50
Erik Krieg <erik.c.krieg@gmail.com>
2015-05-02 10:17:45 -0400

    created trollObject script. added a tree... oops.

---
+++ Vac u me/Assets/trollObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Holoville.HOTween;
+
+public class trollObject : ObjectScript {
+
+	public float speed = 2;
+
+	private GameObject target;
+
+	public override void Start(){
+		base.Start ();
+		body.constraints = RigidbodyConstraints.FreezeRotationY;
+
+		//target = Game.all [Random.Range(0, Game.all.Count - 1)];
+	}
+
+	// selects a target object
+
+	// moves to object
+		// destroys object
+		// selects new object
+}
