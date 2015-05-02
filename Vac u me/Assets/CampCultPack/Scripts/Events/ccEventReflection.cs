9032041802112515cbe5937c84f113618f344e21
netgrind <cale.bradbury@gmail.com>
2015-05-02 00:36:43 -0400

    added field randomization on event

---
+++ Vac u me/Assets/CampCultPack/Scripts/Events/ccEventReflection.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Reflection;
+
+public class ccEventReflection : ccEventBase {
+
+	public string varName;
+	public Component obj;
+	public FieldInfo field;
+	float v = 0;
+	float t = 0;
+
+	protected override void OnEnable(){
+		field = obj.GetType ().GetField (varName);
+		base.OnEnable ();
+	}
+
+	protected override void OnEvent (){
+		field.SetValue (obj, GetValue());
+	}
+
+	protected virtual object GetValue(){
+		return null;
+	}
+}
---
+++ Vac u me/Assets/ccEventRandomizeFloat.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ccEventRandomizeFloat : ccEventReflection {
+
+	public float minValue = -1;
+	public float maxValue = 1;
+
+	protected override object GetValue (){
+		return Random.Range (minValue, maxValue);
+	}
+}
