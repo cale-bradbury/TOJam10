b0e8f1878d4566558555c9d720055bac4b5203b8
netgrind <cale.bradbury@gmail.com>
2015-05-03 14:03:23 -0400

    lot of shit

---
+++ Vac u me/Assets/CouchScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CouchScript : MonoBehaviour {
+	public GameObject surface;
+	public Color[] colors;
+
+	Material mat;
+	Material childMat;
+
+	void Start(){
+		mat = GetComponent<Renderer> ().material;
+		childMat = surface.GetComponent<Renderer> ().material;
+		Randomize ();
+	}
+
+	// Use this for initialization
+	void Randomize () {
+		mat.color = childMat.color = ArrayUtils.getRandom(colors);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Vac u me/Assets/NozzleScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NozzleScript : MonoBehaviour {
+
+	public Transform lookTarget;
+	public float lerp = .1f;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Quaternion v = transform.localRotation;
+		transform.LookAt (lookTarget.position);
+		v = Quaternion.Lerp(v,transform.localRotation,lerp);
+		transform.localRotation = v;
+	}
+}
