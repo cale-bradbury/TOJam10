1941706a9e88e4d37321b1535308f5360cba95f3
netgrind <cale.bradbury@gmail.com>
2015-05-02 20:31:30 -0400

    updated vacscript

--- Vac u me/Assets/VacScript.cs
+++ Vac u me/Assets/VacScript.cs
@@ -9,11 +9,13 @@ public class VacScript : MonoBehaviour {
 	public string horiAxis;
 	public string vertAxis;
 
+	public float suctionDistance = 1;
 	public float suctionPower = 1;
 
 	public float collectDistance = .5f;
 	public float moveDistancePerSecond = 10;
 	public float positionLerp = .1f;
+	public GameObject suctionObject;
 	Vector3 targetPosition;
 	float dir;
 
@@ -22,7 +24,7 @@ public class VacScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
@@ -44,18 +46,24 @@ public class VacScript : MonoBehaviour {
 			ndir += 1;
 		if (Input.GetButton (blowInput))
 			ndir -= 1;
-		dir = Mathf.Lerp (dir,ndir,.05f);
+		dir = Mathf.Lerp (dir,ndir,.1f);
+		suctionObject.transform.localScale = Vector3.one * suctionDistance * dir;
 		foreach (ObjectScript o in Game.all) {
-			if(!o.collected&&dir>.7f){
-				float d = Vector3.Distance(transform.position,o.transform.position);
-				if(d<collectDistance){
-					Collect(o);
+			if (!o.collected ) {
+				if(dir > .7f){
+					float d = Vector3.Distance (transform.position, o.transform.position);
+					o.transform.localScale = Vector3.one * Mathf.Min (1, Mathf.Max (0, d - collectDistance) * 2);
+					if (d < collectDistance) {
+						Collect (o);
+					}
+					d = Mathf.Lerp (1, 0, Mathf.Min (d / suctionDistance, 1)) * ndir * suctionPower;
+					o.body.AddForce ((transform.position - o.transform.position) * d, ForceMode.Force);
+				} else {
+					o.transform.localScale = Vector3.one;
 				}
-				d = Mathf.Max(suctionPower-d,0)*ndir;
-				o.body.AddForce((transform.position-o.transform.position)*d);
 			}
 		}
-		Shader.SetGlobalVector("_vac",new Vector4(transform.position.x,transform.position.y,transform.position.z,Mathf.Max(0,suctionPower*dir)));
+		Shader.SetGlobalVector("_vac",new Vector4(transform.position.x,transform.position.y,transform.position.z,Mathf.Max(0,suctionDistance*dir)));
 	}
 
 	void Collect(ObjectScript o){
